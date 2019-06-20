ad428ac7fa6475bd014d56974b79929fabbbdf0c
marylinkana <kanabaudoin7@gmail.com>
2019-06-20 18:55:17 +0200

    housse connected project

---
+++ house-connected/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  isAuth = false;
+
+  constructor() {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    );
+  }
+
+  onAllumer() {
+    console.log('On allume tout !');
+  }
+
+  onEteindre() {
+    console.log('On eteint tout !');
+}
+}
---
+++ house-connected/src/app/appareil/appareil.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-appareil',
+  templateUrl: './appareil.component.html',
+  styleUrls: ['./appareil.component.scss']
+})
+export class AppareilComponent implements OnInit {
+
+  appareilName: string = "Machine à laver";
+  appareilStatus: string = "Eteint";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getStatus() {
+    return this.appareilStatus;
+  }
+
+}
